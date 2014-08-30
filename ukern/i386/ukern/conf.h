a42b7a02efb3bdbafbc6f8a100fffe799254afa3
Gianluca Guida <glguida@tlbflush.org>
2014-08-30 17:52:09 +0100

    cleanup boot.S and use temporary pages at boot

--- ukern/i386/ukern/conf.h
+++ ukern/i386/ukern/conf.h
@@ -10,11 +10,13 @@
 #define OFF16_ADDR(_a) ((_a) & 0xffff)
 
 /* Temporary address for boot-time allocation. */
-#define ADDR_CODE16 0x10000 /* 16bit code  */
-#define ADDR_STCK16 0x2fffe /* 16bit stack */
-#define ADDR_SMAP16 0x30000 /* E820 map */
+#define ADDR_CODE16   0x10000 /* 16bit code  */
+#define ADDR_STCK16   0x2fffe /* 16bit stack */
+#define ADDR_SMAP16   0x30000 /* E820 map */
 
-#define ADDR_BOOTL3 0x50000 /* L3 table for boot (32 bytes) */
-#define ADDR_BOOTL2 0x51000 /* L2 table for boot (4k bytes) */
+#define ADDR_BOOTL3   0x50000 /* L3 table for boot (32 bytes) */
+#define ADDR_BOOTL2   0x51000 /* L2 table for boot (4k bytes) */
+#define ADDR_BOOTGDTR 0x52000 /* boot GDTR  */
+#define ADDR_BOOTGDT  0x52030 /* boot GDT */
 
 #endif
