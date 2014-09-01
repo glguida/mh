2f900b0817a0578a13d2430ad9533cfdc713ff44
Gianluca Guida <glguida@tlbflush.org>
2014-09-02 00:48:06 +0100

    lib: add find last bit function

--- ukern/lib/lib.h
+++ ukern/lib/lib.h
@@ -4,10 +4,13 @@
 #include <uk/cdefs.h>
 #include <uk/stdarg.h>
 
+#define panic(_s) do { printf("PANIC: %s\n", _s); while(1); } while(0)
+
 int (*putc)(int);
 void *memset(void *b, int c, size_t len);
 int vprintf(const char *fmt, va_list ap);
 int printf(const char *, ...) __printflike(1, 2);
 void _setputcfn(int(*)(int));
+int fls(int);
 
 #endif
