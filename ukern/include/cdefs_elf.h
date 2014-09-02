ce8a3fe356c57dab5b0a3b7ab53773f9c2ae9cc8
Gianluca Guida <glguida@tlbflush.org>
2014-09-02 14:45:45 +0100

    cdefs_elf: better alias definitions

--- ukern/include/cdefs_elf.h
+++ ukern/include/cdefs_elf.h
@@ -3,8 +3,15 @@
 
 #include <machine/uk/asm.h>
 
-#define __weak_alias(alias,sym)			\
-  __asm(".weak " _C_LABEL_STRING(alias) "\n"	\
+#define __decl_weak_alias(alias,sym)				\
+  __asm(".weak " _C_LABEL_STRING(alias) "\n"			\
 	_C_LABEL_STRING(alias) " = " _C_LABEL_STRING(sym))
+
+#define __decl_alias(alias,sym)					\
+  __asm(".globl " _C_LABEL_STRING(alias) "\n"			\
+	_C_LABEL_STRING(alias) " = " _C_LABEL_STRING(sym))
+
+#define __weak_alias(x) __attribute__((weak, alias (#x)))
+#define __alias(x) __attribute__((alias (#x)))
 
 #endif
