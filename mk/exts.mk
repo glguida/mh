4b747f4ac3cf742153a1e9aa1d69f97fc9766ca9
Gianluca Guida <glguida@tlbflush.org>
2014-09-08 13:15:29 +0100

    mk: don't be verbose on untarring

--- mk/exts.mk
+++ mk/exts.mk
@@ -1,5 +1,5 @@
 exts:
 	for i in $(EXTS); do rm -rf $(EXTSRCDIR)/$$i; done
-	for i in $(EXTS); do (cd $(EXTSRCDIR); $(TAR) xvzf $$i.tar.gz; $(PATCH) -p0 < $$i.patch;); done
+	for i in $(EXTS); do (cd $(EXTSRCDIR); $(TAR) xzf $$i.tar.gz; $(PATCH) -p0 < $$i.patch;); done
 
 ALL_TARGET+= exts
