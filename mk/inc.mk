96bb8adafe552a444c9bd9472775b2795a011475
Gianluca Guida <glguida@tlbflush.org>
2015-02-03 10:33:23 +0000

    mk/inc.mk: make more modular

--- mk/inc.mk
+++ mk/inc.mk
@@ -1,7 +1,20 @@
-.PHONY: all includes clean
+.PHONY: all includes clean incdir incs
 ALL_TARGET+= includes
 
-includes:
-	for dir in $(INCSUBDIRS); do $(MAKE) -C $$dir includes; done
+ifneq ($(INCDIR)z,z)
+ALL_TARGET+= incdir
+endif
+
+ifneq ($(INCS)z,z)
+ALL_TARGET+= incs
+endif
+
+incdir:
 	install -d ${INSTALLINCDIR}/${INCDIR}
+
+incs:
 	install -m 0644 ${INCS} ${INSTALLINCDIR}/${INCDIR}/ 
+
+
+includes:
+	for dir in $(INCSUBDIRS); do $(MAKE) -C $$dir; done
