a5f105f4fac620fb9554cd4039d93bb9e32de972
Gianluca Guida <glguida@tlbflush.org>
2015-02-03 10:09:44 +0000

    ukern: add usrentry (not enabled yet)

---
+++ ukern/i386/ukern/__usrentry.c
@@ -0,0 +1,49 @@
+#include <uk/types.h>
+#include <machine/uk/param.h>
+#include <machine/uk/cpu.h>
+#include <ukern/thread.h>
+#include <ukern/usrentry.h>
+#include <lib/lib.h>
+
+void
+__usrentry_enter(void *frame)
+{
+  void ___usrentry_enter(void *frame);
+
+    current_cpu()->tss.esp0 = (uint32_t)current_thread()->stack_4k + 0xff0;
+    current_cpu()->tss.ss0 = KDS;
+    ___usrentry_enter(frame);
+}
+
+void
+__usrentry_setup(struct usrentry *ue, vaddr_t ip)
+{
+    unsigned long *ptr = (unsigned long *)ue->data;
+
+    memset(ue->data, 0, 17 * 4);
+    ptr[0] = (UDS << 16) | UDS; /* DS, ES */
+    ptr[1] = (UDS << 16) | UDS; /* FS, GS */
+
+    /*
+     * ptr[2]: cr3;
+     * ptr[3]: cr2;
+     * ptr[4]: edi;
+     * ptr[5]: esi;
+     * ptr[6]: ebp;
+     * ptr[7]: esp (ign);
+     * ptr[8]: ebx;
+     * ptr[9]: edx;
+     * ptr[10]: ecx;
+     * ptr[11]: eax;
+     * ptr[12]: error (fake);
+     */
+    
+    /* IRET FRAME */
+    ptr[13] = ip;    /* EIP */
+    ptr[14] = UCS;   /* CS */
+    ptr[15] = 0x202; /* EFLAGS */
+    ptr[16] = 0;     /* ESP */
+    ptr[17] = UDS;   /* SS */
+}
+
+
--- ukern/ukern/thread.h
+++ ukern/ukern/thread.h
@@ -7,25 +7,22 @@
 
 #define _THFL_STOPPED 0
 #define THFL_STOPPED (1 << _THFL_STOPPED)
-
-#define _THFL_THEPOFF 1
-#define THFL_THEPOFF (1 << _THFL_THEPOFF)
-#define _THFL_THEPLD  2
-#define THFL_THEPLD  (1 << _THFL_THEPLD)
-#define _THFL_THEPON  3
-#define THFL_THEPON  (1 << _THFL_THEPON)
+#define _THFL_IN_USRENTRY 1
+#define THFL_IN_USRENTRY (1 << _THFL_IN_USRENTRY)
 
 #define thread_is_runnable(_th) (!((_th)->flags & THFL_STOPPED))
 #define thread_is_idle(_th) (_th == current_cpu()->idle_thread)
 
 struct thread {
-    jmp_buf        ctx;
-    struct addrspc *addrspc;
-    void           *stack_4k;
+    jmp_buf         ctx;
+    struct addrspc  *addrspc;
+    struct usrentry *usrentry;
+    void            *stack_4k;
+    void            *frame;
 
-    struct thep    *current;
+    struct thep     *current;
 
-    uint32_t            flags;
+    uint32_t        flags;
     TAILQ_ENTRY(thread) sched_list;
 };
 
