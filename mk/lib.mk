46ed8949bed95871d029611eb71632b33d0d829d
Gianluca Guida <glguida@tlbflush.org>
2015-11-02 15:58:30 -0500

    mk: fix NOINST for libs

--- mk/lib.mk
+++ mk/lib.mk
@@ -1,16 +1,17 @@
 .PHONY: clean_lib$(LIBNAME) install_lib$(LIBNAME)
 ALL_TARGET+= install_lib$(LIBNAME)
 CLEAN_TARGET+= clean_lib$(LIBNAME)
-ifeq ($(NOINST)z, z)
-	INSTALL_TARGET+= install_lib$(LIBNAME)
-endif
 
 lib$(LIBNAME).a: $(OBJS)
 	$(AR) r $@ $(OBJS)
 
 install_lib$(LIBNAME): lib$(LIBNAME).a
+ifeq ($(NOINST)z, z)
 	install -d $(INSTALLDIR)/$(LIBDIR)
 	install -m 0644 lib$(LIBNAME).a  $(INSTALLDIR)/$(LIBDIR)/lib$(LIBNAME).a
+endif
+
+
 
 clean_lib$(LIBNAME):
 	-rm $(OBJS) lib$(LIBNAME).a
