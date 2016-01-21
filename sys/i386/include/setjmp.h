4ab39604b96f871c2515535362827af7afb08512
Gianluca Guida <glguida@tlbflush.org>
2016-01-21 02:41:32 +0000

    licsys: add _setjmp/_longjmp

---
+++ sys/i386/include/setjmp.h
@@ -0,0 +1,8 @@
+/* *INDENT-OFF* */ /* Imported from NetBSD -- MHDIFFIGNORE */
+/*	$NetBSD: setjmp.h,v 1.2 1998/09/14 21:31:52 thorpej Exp $	*/
+
+/*
+ * machine/setjmp.h: machine dependent setjmp-related information.
+ */
+
+#define	_JBLEN	13		/* size, in longs, of a jmp_buf */
