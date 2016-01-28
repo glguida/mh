3feec8de86c25bb0c0b8ab2290ead61da25952a7
Gianluca Guida <glguida@tlbflush.org>
2016-01-28 11:11:40 +0000

    libdrex: fix warnings

--- sys/libdrex/i386/machdep.c
+++ sys/libdrex/i386/machdep.c
@@ -38,7 +38,7 @@ lwt_makebuf(lwt_t *lwt, void (*start)(void *), void * arg,
 	void *sp = (void *)(stack_base + stack_size - 12);
 
 	*(void **)(sp + 8) = arg;
-	*(void **)(sp + 4) = 0x50500505; /* Canary for return value */
+	*(void **)(sp + 4) = (void*)0x50500505; /* Canary for return value */
 	*(void **)sp = 0; /* _longjmp uses ret, which consumes this */
 	_setupjmp(lwt->buf, (void *)start, sp);
 }
